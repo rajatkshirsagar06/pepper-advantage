669f4ca9e2917ab1b18e3465455b185f0c034df2
rajatkshirsagar06 <rajatkshirsagar06@gmail.com>
2024-12-14 15:49:30 +0530

    Updates

---
+++ pepper-advantage/assets/mock-data/mock-data.ts
@@ -0,0 +1,52 @@
+export const mockUsers = [
+  {
+    userId: 'rajatKshirsagar@pepperadvantage.in',
+    password: 'Rajat@123',
+    name: 'Rajat',
+  },
+  {
+    userId: 'bhavinpatel@pepperadvantage.in',
+    password: 'Bhavin@123',
+    name: 'Bhavin',
+  },
+];
+
+export const feedback = {
+  feedbacks: [
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut convallis ipsum. Praesent a massa tincidunt, euismod turpis sed, rutrum arcu. Nulla eu maximus dui. Cras pulvinar fringilla mi, nec cursus sem ullamcorper at. Vestibulum ut ex ante. Nulla porta arcu a ex placerat commodo. Integer scelerisque sodales lectus eget laoreet. Fusce sit amet mi id risus condimentum commodo.',
+      rating: 2,
+      date: '14/06/2023',
+      starCount: 5,
+    },
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut convallis ipsum. Praesent a massa tincidunt, euismod turpis sed, rutrum arcu. Nulla eu maximus dui. Cras pulvinar fringilla mi, nec cursus sem ullamcorper at.',
+      rating: 3,
+      date: '14/06/2023',
+      starCount: 5,
+    },
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut convallis ipsum. Praesent a massa tincidunt, euismod turpis sed, rutrum arcu. Nulla eu maximus dui. Cras pulvinar fringilla mi, nec cursus sem ullamcorper at. Vestibulum ut ex ante. Nulla porta arcu a ex placerat commodo. Integer scelerisque sodales lectus eget laoreet. Fusce sit amet mi id risus condimentum commodo.',
+      rating: 1,
+      date: '14/06/2023',
+      starCount: 5,
+    },
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut convallis ipsum. Praesent a massa tincidunt, euismod turpis sed, rutrum arcu. Nulla eu maximus dui. Cras pulvinar fringilla mi, nec cursus sem ullamcorper at. Vestibulum ut ex ante. Nulla porta arcu a ex placerat commodo. Integer scelerisque sodales lectus eget laoreet. Fusce sit amet mi id risus condimentum commodo.',
+      rating: 5,
+      date: '14/06/2023',
+      starCount: 5,
+    },
+    {
+      comment:
+        '“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut convallis ipsum. Praesent a massa tincidunt, euismod turpis sed, rutrum arcu. Nulla eu maximus dui. Cras pulvinar fringilla mi, nec cursus sem ullamcorper at.”',
+      rating: 1,
+      date: '14/06/2023',
+      starCount: 5,
+    },
+  ],
+};
